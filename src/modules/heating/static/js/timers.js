ea091e4eb69a7ce183000f9cc3249d7944a47471
Cameron Gray <cameron@camerongray.me>
2018-06-21 23:51:10 +0100

    Added logic to serialise timer form

--- src/modules/heating/static/js/timers.js
+++ src/modules/heating/static/js/timers.js
@@ -10,8 +10,27 @@ jQuery(document).ready(function() {
 
     function toggleDays(clickedElement, dayType) {
         jQuery(clickedElement).closest(".day-button-group").find("label").removeClass("active");
-        jQuery(clickedElement).closest(".day-button-group").find("label > checkbox").removeAttr("checked");
+        jQuery(clickedElement).closest(".day-button-group").find("label > input").prop("checked", false);
         jQuery(clickedElement).closest(".day-button-group").find("label[data-type='" + dayType + "']").addClass("active");
-        jQuery(clickedElement).closest(".day-button-group").find("label[data-type='" + dayType + "'] > checkbox").attr("checked", "checked");
+        jQuery(clickedElement).closest(".day-button-group").find("label[data-type='" + dayType + "'] > input").prop("checked", true);
     }
+
+    function serialiseTimerForm() {
+        var timers = [];
+        jQuery("#timers-form .timer-row").each(function() {
+            var timer = {};
+            timer["startTime"] = jQuery(this).find("[name='start-time']").val();
+            timer["endTime"] = jQuery(this).find("[name='end-time']").val();
+            timer["temperature"] = jQuery(this).find("[name='temperature']").val();
+            
+            timer["days"] = {}
+            jQuery(this).find("[name='day-checkbox']").each(function() {
+                timer["days"][jQuery(this).val()] = jQuery(this).is(":checked");
+            });
+
+            timers.push(timer);
+        });
+        return timers;
+    }
+    window.serialiseTimerForm = serialiseTimerForm;
 });
